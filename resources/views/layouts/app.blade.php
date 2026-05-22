ed8b04b0d63a73f194c7562eac1088ae2ba39866
PJM012 <patrickmaniaul12@gmail.com>
2026-05-22 10:06:07 +0800

    -change icon png

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" type="image/x-icon" href="{{ asset('tspc-icon.ico') }}">
+    <link rel="icon" type="image/png" href="{{ asset('tspc-icon.png') }}">
     <title>Toyoseat - @yield('title', 'Home')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
